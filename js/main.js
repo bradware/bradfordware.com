24c21c40632a250ca1c1b226a54ccb86f20fc278
Brad Ware <bradware@fb.com>
2020-04-04 20:59:07 -0700

    dark mode

--- js/main.js
+++ js/main.js
@@ -4,25 +4,26 @@ const BACKGROUND_COLOR = `background-color`;
 const WHITE = 'white';
 const BLACK = 'black';
 
+var IS_DARK_MODE = false;
+
 $(document).ready(function() {
 	// const content = $(`#content`);
 	// content.css('marginTop', -content.height() + 'px');
 	
-	let isDarkMode = false;
-	const page = $('body,html,main');
-	
-	$(page).click(function() {
-    isDarkMode = !isDarkMode;
-    changeColorScheme(page, isDarkMode);
+	const elements = $('body,html,main');
+	$(elements).click(function() {
+    toggleColorScheme(elements);
 	});
 });
 
-function changeColorScheme(page, isDarkMode) {
-	if (isDarkMode) {
-			page.css(COLOR, WHITE);
-    	page.css(BACKGROUND_COLOR, BLACK);
-    } else {
-    	page.css(COLOR, BLACK);
-    	page.css(BACKGROUND_COLOR, WHITE);
-    }
+function toggleColorScheme(elements) {
+	IS_DARK_MODE = !IS_DARK_MODE;
+	
+	if (IS_DARK_MODE) {
+		elements.css(COLOR, WHITE);
+  	elements.css(BACKGROUND_COLOR, BLACK);
+  } else {
+  	elements.css(COLOR, BLACK);
+  	elements.css(BACKGROUND_COLOR, WHITE);
+  }
 }
