8250dd6a6e599fef4851532ca9d4cd694d1c3ee7
Brad Ware <bradware@fb.com>
2020-04-05 20:49:16 -0700

    code cleanup

--- js/main.js
+++ js/main.js
@@ -3,17 +3,18 @@ const COLOR = `color`;
 const BACKGROUND_COLOR = `background-color`;
 const WHITE = 'white';
 const BLACK = 'black';
-const ICON_TAG_NAME = 'I';
 
+// File Scoped
 let IS_DARK_MODE = false;
 
 $(document).ready(function() {
-	const content = $(`main`);
-	content.css('marginTop', -content.height() + 'px');
+	const main = $(`main`);
+	main.css('marginTop', -main.height() + 'px');
 	
 	const html = $('html');
 	$(html).click(function(event) {
-    if (event.target.tagName === ICON_TAG_NAME) {
+		// Don't change the color scheme if clicking on an icon
+    if (event.target.tagName === 'I') {
     	return;
     }
     
