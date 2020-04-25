a6260e7b2a8513ccc7ddf92d16eb9ded95def469
Brad Ware <bradware@fb.com>
2020-04-25 13:16:07 -0700

    added loading animation for font to load before showing

--- js/main.js
+++ js/main.js
@@ -1,21 +1,32 @@
 // Constants
 const HTML = 'html';
 const BODY = 'body';
+const LOADING = '#loading';
+const CONTENT = '#content';
+const PX = 'px';
+const ICON = 'I';
+
 const COLOR = 'color';
 const BACKGROUND_COLOR = 'background-color';
 const WHITE = 'white';
 const BLACK = 'black';
 const MARGIN_TOP = 'marginTop';
-const PX = 'px';
-const ICON = 'I';
+
+const ONE_SECOND = 1000;
+const CONTENT_OFFSET = 2;
 const HEIGHT_OFFSET = 0.75;
 
 let IS_DARK_MODE = true;
 
 $(document).ready(function() {
-	const html = $(HTML);
+	const loading = $(LOADING);
+	const content = $(CONTENT);
 	const body = $(BODY);
-	
+	const html = $(HTML);
+
+	loading.delay(ONE_SECOND).fadeOut(ONE_SECOND);
+	content.delay(ONE_SECOND * CONTENT_OFFSET).fadeIn(ONE_SECOND);
+
 	body.css(MARGIN_TOP, -(body.height() * HEIGHT_OFFSET) + PX);
 	html.click(function(event) {
 		// Don't change the color scheme if clicking on an icon
