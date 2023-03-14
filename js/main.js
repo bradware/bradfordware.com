11903e75d6f76d8d323f41f74fb5dfb64de541cf
Brad Ware <bradware@fb.com>
2023-03-14 13:57:00 -0700

    added light and dark mode for favicon

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Constants
 const HTML = 'html';
 const BODY = 'body';
+const FAVICON = '#favicon';
 const LOADING = '#loading';
 const CONTENT = '#content';
 const PX = 'px';
@@ -11,6 +12,9 @@ const BACKGROUND_COLOR = 'background-color';
 const WHITE = 'white';
 const BLACK = 'black';
 const MARGIN_TOP = 'marginTop';
+const HREF = 'href';
+const FAVICON_DARK = 'assets/favicon_dark.png';
+const FAVICON_LIGHT = 'assets/favicon_light.png';
 
 const ONE_SECOND = 1000;
 const LOADING_OFFSET = ONE_SECOND * 2;
@@ -24,6 +28,7 @@ $(document).ready(function() {
 	const content = $(CONTENT);
 	const body = $(BODY);
 	const html = $(HTML);
+	const favicon = $(FAVICON);
 
 	loading.delay(ONE_SECOND).fadeOut(ONE_SECOND);
 	content.delay(LOADING_OFFSET).fadeIn(ONE_SECOND);
@@ -32,7 +37,7 @@ $(document).ready(function() {
 	html.click(function(event) {
 		// Ignore if clicking on an icon (external link) or the content hasn't loaded yet
 		if (isTargetEligible(event.target) && isTimeEligible()) {
-			toggleColorScheme(body);
+			toggleColorScheme(body, favicon);
 		}
 	});
 });
@@ -45,14 +50,16 @@ function isTimeEligible() {
 	return $.now() - START_TIME > LOADING_OFFSET;
 }
 
-function toggleColorScheme(domElement) {
+function toggleColorScheme(domElement, favicon) {
 	IS_DARK_MODE = !IS_DARK_MODE;
 
 	if (IS_DARK_MODE) {
 		domElement.css(COLOR, WHITE);
 		domElement.css(BACKGROUND_COLOR, BLACK);
+		favicon.attr(HREF, FAVICON_DARK);
 	} else {
 		domElement.css(COLOR, BLACK);
 		domElement.css(BACKGROUND_COLOR, WHITE);
+		favicon.attr(HREF, FAVICON_LIGHT);
 	}
 }
